6ef6731f6f470aecee6671b9b0e1fbcf0b3062eb
Sankalp-G <sankalp.gawali@gmail.com>
2024-03-27 23:54:10 +0530

    add nvim specific padding script

--- home-manager/terminal/wezterm/default.nix
+++ home-manager/terminal/wezterm/default.nix
@@ -3,7 +3,27 @@
     enable = true;
 
     extraConfig = ''
-      local wezterm = require "wezterm"
+      local padding = {
+        left = '1cell',
+        right = '1cell',
+        top = '0.5cell',
+        bottom = '0.5cell',
+      }
+
+      wezterm.on('update-status', function(window, pane)
+        local overrides = window:get_config_overrides() or {}
+        if string.find(pane:get_title(), 'NVIM') then
+          overrides.window_padding = {
+            left = 0,
+            right = 0,
+            top = 0,
+            bottom = 0
+          }
+        else
+          overrides.window_padding = padding
+        end
+        window:set_config_overrides(overrides)
+      end)
 
       return {
         check_for_updates = false,
@@ -14,13 +34,7 @@
         hide_tab_bar_if_only_one_tab = true,
         scrollback_lines = 10000,
         window_background_opacity = 0.9,
-        font = wezterm.font('JetBrains Mono', { weight = 'Medium' }),
-        window_padding = {
-          left = 0,
-          right = -2,
-          top = 0,
-          bottom = 0,
-        },
+        font = wezterm.font('JetBrains Mono', { weight = 'Medium' })
       }
     '';
   };
