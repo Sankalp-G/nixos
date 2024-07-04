dae1f83d9003919218480cfcdbd2d4e6dc3eee37
Sankalp-G <sankalp.gawali@gmail.com>
2024-07-04 18:53:35 +0530

    update kitty config remove neovim submodule

--- home-manager/terminal/kitty.nix
+++ home-manager/terminal/kitty.nix
@@ -12,8 +12,10 @@
     theme = "Everforest Dark Hard";
 
     extraConfig = ''
-      window_margin_width 8
+      window_margin_width 6
       hide_window_decorations yes
+      draw_minimal_borders yes
+      enable_audio_bell no
     '';
   };
 }
