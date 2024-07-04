4066603e81d9f273f83cdf19072901ed6c629f5c
Sankalp-G <sankalp.gawali@gmail.com>
2024-07-04 19:24:28 +0530

    update kitty config

--- home-manager/terminal/kitty.nix
+++ home-manager/terminal/kitty.nix
@@ -12,10 +12,17 @@
     theme = "Everforest Dark Hard";
 
     extraConfig = ''
-      window_margin_width 6
+      window_padding_width 6
       hide_window_decorations yes
       draw_minimal_borders yes
       enable_audio_bell no
+      kitty_mod alt
+      background_opacity 0.9
+
+      map ctrl+shift+v paste_from_clipboard
+      map ctrl+shift+c copy_to_clipboard
+      map kitty_mod+enter launch --cwd=current
+      map kitty_mod+t launch --cwd=current --type=tab
     '';
   };
 }
