3dc080cb190e4420ff33d92b592f626917034cd2
Sankalp-G <sankalp.gawali@gmail.com>
2025-08-13 23:46:37 +0530

    feat: remove prompt reset

--- home-manager/terminal/zsh.nix
+++ home-manager/terminal/zsh.nix
@@ -94,7 +94,6 @@
 	      yazi "$@" --cwd-file="$tmp"
 	      if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
 		      cd -- "$cwd"
-                      zle reset-prompt
 	      fi
 	      rm -f -- "$tmp"
       }
